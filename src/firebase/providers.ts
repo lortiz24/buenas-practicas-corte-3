514774bf4fc8f5208a3dc78bea7d890d2f8b04b9
lortiz24 <luis.ortiz@evius.co>
2023-05-29 22:34:23 -0500

    Se crea provider de manillas

--- src/firebase/providers.ts
+++ src/firebase/providers.ts
@@ -2,9 +2,11 @@ import { collection, CollectionReference } from "firebase/firestore";
 import { FirebaseDB } from "./ConfigFirebase";
 import { Material } from "../interface/materiales.interface";
 import { Dije } from "../interface/dije.interface";
+import { Manilla } from "../interface/manilla.interface";
 
 export const materialesCollectionRef = collection(FirebaseDB, "materiales") as CollectionReference<Omit<Material, 'id'>>;
 export const dijesCollectionRef = collection(FirebaseDB, "dijes") as CollectionReference<Omit<Dije, 'id'>>;
+export const manillasCollectionRef = collection(FirebaseDB, "manillas") as CollectionReference<Omit<Manilla, 'id'>>;
 
 
 
